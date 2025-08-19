acaa2dc9faf649080022050c3d75a0acfddf6f17
stav1019 <stavsendrovitz@gmail.com>
2025-08-18 22:17:34 -0700

    added styles part 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../components/auth/AuthContext';
-import '../styles/app.css'; 
+import '../styles/App.css'; 
 
 export default function App({ Component, pageProps }) {
   return (
