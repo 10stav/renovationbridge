3b77af534d94704d8e9d5d453bf863df08a9f626
stav1019 <stavsendrovitz@gmail.com>
2025-08-19 17:38:25 -0700

    fixing images

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
-import { AuthProvider } from '../components/auth/AuthContext';
-import '../styles/index.css';
-
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-600 via-purple-600 to-blue-800">
+    // Remove this wrapper temporarily:
+    // <div className="min-h-screen bg-gradient-to-br from-blue-600 via-purple-600 to-blue-800">
       <AuthProvider>
         <Component {...pageProps} />
       </AuthProvider>
-    </div>
+    // </div>
   );
 }
