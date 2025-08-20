c17b2b50f02981fcee65bcbdbe644edd9e3508a8
stav1019 <stavsendrovitz@gmail.com>
2025-08-19 17:49:36 -0700

    fixing images

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,8 @@ import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-600 via-purple-600 to-blue-800">
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </div>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
